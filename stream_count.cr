23159436178dcfc0c45f8f13891cb761effdff93
Scott Pierce <scott.pierce@centro.net>
2017-08-29 09:45:44 -0500

    Fixes for Issue #1
    We were writing too much of the data slice.

--- stream_count.cr
+++ stream_count.cr
@@ -3,8 +3,7 @@
 #
 # rubocop:disable all
 class StreamCount
-
-  BUFFER_SIZE   = 1024
+  BUFFER_SIZE   = 10240
   TICK_DURATION = 20000 # microsecond
 
   @start_time : Int64
@@ -18,42 +17,53 @@ class StreamCount
 
   # Do the work
   def run
-    bytes = 0
+    total_bytes = 0
+    read_bytes = 0
     lines = 0
-    output(bytes: bytes, lines: lines, throttle: false)
-    while (@io.read(slice: @slice) > 0)
-      STDOUT.write(@slice)
-      bytes += @slice.size
-      @slice.each do |c|
-        if c == 0x0A 
-          lines += 1
-        end
-      end
-      # lines += new_lines.size # @slice.count("\n")
-      output(bytes: bytes, lines: lines, throttle: true)
+    output(bytes: total_bytes, lines: lines)
+    while (read_bytes = @io.read_utf8(slice: @slice)) > 0
+      total_bytes += read_bytes
+      lines += count_new_lines(@slice)
+      STDOUT.write(@slice[0, read_bytes])
+      throttler { output(bytes: total_bytes, lines: lines) }
     end
-    output(bytes: bytes, lines: lines, throttle: false)
+    STDOUT.flush
+    output(bytes: total_bytes, lines: lines)
+  end
+
+  def count_new_lines(slice)
+    result = 0
+    slice.each do |c|
+      if c == 0x0A
+        result += 1
+      end
+    end
+    result
   end
 
   # output formatted stats to stderr.
   # Using throttle will limit how often we print to stderr to 5/second.
-  def output(bytes, lines, throttle=true)
-    throttler(force: !throttle) do
-      msg = "\e[1G\e[2K%s seconds | %s kb [ %s kb/sec ] | %s lines [ %s lines/sec ]"
-      duration = Time.utc_ticks - @start_time
-      if duration > 0
-        STDERR.print(msg % [number_with_delimiter(duration),
-                            number_with_delimiter((bytes / 1024)),
-                            number_with_delimiter((bytes / duration / 1024).to_i),
-                            number_with_delimiter(lines),
-                            number_with_delimiter((lines / duration).to_i)])
-      end
+  def output(bytes, lines)
+    msg = "\e[1G\e[2K%s seconds | %s kb [ %s kb/sec ] | %s lines [ %s lines/sec ]"
+    seconds = (Time.utc_ticks - @start_time) / Time::Span::TicksPerSecond
+    if seconds > 0
+      STDERR.print(msg % [number_with_delimiter(seconds),
+                          number_with_delimiter(bytes / 1024),
+                          number_with_delimiter(bytes / seconds / 1024),
+                          number_with_delimiter(lines),
+                          number_with_delimiter(lines / seconds)])
+    else
+      STDERR.print(msg % [number_with_delimiter(seconds),
+                          number_with_delimiter(bytes / 1024),
+                          "?",
+                          number_with_delimiter(lines),
+                          "?"])
     end
   end
 
-  def throttler(force = true, &block)
-    if force || Time.utc_ticks > (@last_tick + TICK_DURATION)
-      block.call
+  def throttler
+    if Time.utc_ticks > (@last_tick + TICK_DURATION)
+      yield
       @last_tick = Time.utc_ticks
     end
   end
